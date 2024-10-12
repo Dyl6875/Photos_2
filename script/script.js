d40f317e003ed91697c042fb0b50ec900b177a26
Dyl6875 <dydylou24@gmail.com>
2024-10-12 23:10:29 +0200

    ouais ouais ouais

--- script/script.js
+++ script/script.js
@@ -1,28 +1,26 @@
-function openModal(canvas) {
-    const modal = document.getElementById("imageModal");
-    const modalImg = document.getElementById("modalImage");
-    const modalCopyright = document.getElementById("modalCopyright");
-    
-    const ctx = canvas.getContext('2d');
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    
+function openModal(element) {
+    var modal = document.getElementById("imageModal");
+    var modalImg = document.getElementById("modalImage");
     document.body.classList.add("no-scroll"); // Empêche le scroll
     modal.style.display = "flex"; // Modale centrée avec flexbox
-    modalImg.src = canvas.toDataURL(); // Convertir le canvas en image
-    modalCopyright.textContent = '© 2024 - Turbo Thrill'; // Récupérer le copyright
+    modalImg.src = element.src;
 
     // Ajoute un écouteur d'événements pour détecter la touche "Escape"
     document.addEventListener('keydown', handleEscape);
 }
 
 function closeModal() {
-    const modal = document.getElementById("imageModal");
-    modal.style.display = "none"; // Cacher la modale
-    document.body.classList.remove("no-scroll"); // Réactiver le scroll
+    var modal = document.getElementById("imageModal");
+    document.body.classList.remove("no-scroll"); // Réactive le scroll
+    modal.style.display = "none";
+
+    // Supprime l'écouteur d'événements une fois la modale fermée
+    document.removeEventListener('keydown', handleEscape);
 }
 
+// Fonction qui détecte si la touche "Escape" est pressée
 function handleEscape(event) {
     if (event.key === 'Escape') {
-        closeModal(); // Fermer la modale si la touche 'Escape' est pressée
+        closeModal();
     }
 }
